cb09694a7cd9b0fc97489f0d398b7996b36bde4e
3vilguy <2122619+3vilguy@users.noreply.github.com>
2019-08-16 09:10:09 +0100

    Adding expo webview. Trying Trumbowyg and ckeditor inside webview.

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <WebView
+    source={{ uri: 'http://3vil.eu/Notesmaster/trumbowyg-jquery/' }}
+    // source={{ uri: 'http://3vil.eu/Notesmaster/ckeditor-4-full-all/index.html' }}
+    style={{ marginTop: 20 }}
+  />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
